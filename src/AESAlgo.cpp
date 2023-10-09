9f3d840cd03eef70f2a480e671d31c6ad6ff68fd
PhiliaTheCat <2787974051@qq.com>
2023-10-09 11:52:20 +0800

    Decryption Completed and Caliberated

--- src/AESAlgo.cpp
+++ src/AESAlgo.cpp
@@ -19,6 +19,23 @@ namespace ptc
         return t3;
     }
 
+    Block InvRound(const Block &src, const Key &key)
+    {
+        Block t1 = AddRndKey(src, key);
+        Block t2 = InvMixRow(t1);
+        Block t3 = InvShiftCol(t2);
+        Block t4 = InvByteSub(t3);
+        return t4;
+    }
+
+    Block InvFinalRound(const Block &src, const Key &key)
+    {
+        Block t1 = AddRndKey(src, key);
+        Block t2 = InvShiftCol(t1);
+        Block t3 = InvByteSub(t2);
+        return t3;
+    }
+
     Block ByteSub(const Block &src)
     {
         Block res = src;
@@ -57,6 +74,48 @@ namespace ptc
             res.mat[i][1] = a1 ^ Mul(a2, 2) ^ Mul(a3, 3) ^ a4;
             res.mat[i][2] = a1 ^ a2 ^ Mul(a3, 2) ^ Mul(a4, 3);
             res.mat[i][3] = Mul(a1, 3) ^ a2 ^ a3 ^ Mul(a4, 2);
+        }
+        return res;
+    }
+
+    Block InvByteSub(const Block &src)
+    {
+        Block res = src;
+        for (int i = 0; i < 4; i += 1)
+        {
+            for (int j = 0; j < 4; j += 1)
+                res.mat[i][j] = InvS[res.mat[i][j].to_ulong()];
+        }
+        return res;
+    }
+
+    Block InvShiftCol(const Block &src)
+    {
+        Block res = src;
+        for (int i = 1; i < 4; i += 1)
+        {
+            res.mat[0][i] = src.mat[(4 - i) % 4][i];
+            res.mat[1][i] = src.mat[(5 - i) % 4][i];
+            res.mat[2][i] = src.mat[(6 - i) % 4][i];
+            res.mat[3][i] = src.mat[(7 - i) % 4][i];
+        }
+        return res;
+    }
+
+    Block InvMixRow(const Block &src)
+    {
+        Block res;
+        std::bitset<8> a1, a2, a3, a4;
+        for (int i = 0; i < 4; i += 1)
+        {
+            a1 = src.mat[i][0];
+            a2 = src.mat[i][1];
+            a3 = src.mat[i][2];
+            a4 = src.mat[i][3];
+            res.mat[i][0] = Mul(a1, 14) ^ Mul(a2, 11) ^ Mul(a3, 13) ^ Mul(a4, 9);
+            res.mat[i][1] = Mul(a1, 9) ^ Mul(a2, 14) ^ Mul(a3, 11) ^ Mul(a4, 13);
+            res.mat[i][2] = Mul(a1, 13) ^ Mul(a2, 9) ^ Mul(a3, 14) ^ Mul(a4, 11);
+            res.mat[i][3] = Mul(a1, 11) ^ Mul(a2, 13) ^ Mul(a3, 9) ^ Mul(a4, 14);
         }
         return res;
     }
--- src/SimpleAES.cpp
+++ src/SimpleAES.cpp
@@ -15,6 +15,9 @@ int main()
     char res[33];
     cin >> msg >> key;
     Encryption(msg, key, res);
+    cout << res << endl;
+    Decryption(res, key, msg);
+    cout << msg << endl;
     return 0;
 }
 
@@ -31,6 +34,21 @@ void ptc::Encryption(const char msg[33], const char key[33], char res[33])
         m = Round(m, arr[i]);
     m = FinalRound(m, arr[10]);
     m.ToStr(res);
-    cout << res << endl;
+    return;
+}
+
+void ptc::Decryption(const char msg[33], const char key[33], char res[33])
+{
+    Block m(msg);
+    Key k(key);
+    Key arr[11];
+    arr[0] = k;
+    for (int i = 1; i < 11; i += 1)
+        arr[i] = arr[i - 1].Get_New_Key(i);
+    m = InvFinalRound(m, arr[10]);
+    for (int i = 9; i >= 1; i -= 1)
+        m = InvRound(m, arr[i]);
+    m = AddRndKey(m, arr[0]);
+    m.ToStr(res);
     return;
 }
